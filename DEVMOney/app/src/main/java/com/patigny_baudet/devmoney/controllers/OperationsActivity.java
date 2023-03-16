06bb66e33553324a69d0aea9262adf44700c9cdb
Pierre <72155300+Pierre36@users.noreply.github.com>
2023-03-16 09:49:01 +0100

    Initialize activities

---
+++ DEVMOney/app/src/main/java/com/patigny_baudet/devmoney/controllers/OperationsActivity.java
@@ -0,0 +1,16 @@
+package com.patigny_baudet.devmoney.controllers;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.patigny_baudet.devmoney.R;
+
+public class OperationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operations);
+    }
+}
