88de799f956d7e0b1f9533d476902cf2d2c441e2
Pierre <72155300+Pierre36@users.noreply.github.com>
2023-03-22 17:34:20 +0100

    Fill main activity with database information

--- DEVMOney/app/src/main/java/com/patigny_baudet/devmoney/controllers/MainActivity.java
+++ DEVMOney/app/src/main/java/com/patigny_baudet/devmoney/controllers/MainActivity.java
@@ -2,6 +2,7 @@ package com.patigny_baudet.devmoney.controllers;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,11 +10,19 @@ import com.patigny_baudet.devmoney.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    // UI COMPONENTS
+    private Button operationsButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_operations);
+        setContentView(R.layout.activity_main);
 
-        this.startActivity(new Intent(this, OperationsActivity.class));
+        // Initialize UI variables
+        this.operationsButton = findViewById(R.id.activity_main_operations_button);
+
+
+        this.operationsButton.setOnClickListener(v -> this.startActivity(new Intent(this, OperationsActivity.class)));
+
     }
 }
